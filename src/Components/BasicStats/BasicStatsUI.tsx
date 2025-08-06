f2b877094fff41bcf737dd465aa849a195373355
Michael Zewdie <michaelzewdie7@gmail.com>
2025-08-06 16:01:53 -0500

    Deployment changes

--- src/Components/BasicStats/BasicStatsUI.tsx
+++ src/Components/BasicStats/BasicStatsUI.tsx
@@ -8,7 +8,7 @@ interface ChessStatsUIProps {
 }
 
 export default function BasicStatsUI({ username}: ChessStatsUIProps) {
-  const { stats, loading, error} = useChessStats(username);
+  const { stats, loading, error } = useChessStats(username);
 
   if (loading) return (
     <div className="fixed bottom-6 right-6 z-50">
